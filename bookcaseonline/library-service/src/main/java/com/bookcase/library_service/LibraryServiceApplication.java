917ded9ca04cf96472b013c864a36915b9086e5e
burakTuran00 <burakturan7410@gmail.com>
2025-10-28 10:49:00 +0300

    added Bean For Build

--- bookcaseonline/library-service/src/main/java/com/bookcase/library_service/LibraryServiceApplication.java
+++ bookcaseonline/library-service/src/main/java/com/bookcase/library_service/LibraryServiceApplication.java
@@ -1,9 +1,13 @@
 package com.bookcase.library_service;
 
+import com.bookcase.library_service.client.RetreiveMessageErrorDecoder;
+import feign.Logger;
+import feign.codec.ErrorDecoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -14,4 +18,13 @@ public class LibraryServiceApplication {
 		SpringApplication.run(LibraryServiceApplication.class, args);
 	}
 
+	@Bean
+	public ErrorDecoder errorDecoder(){
+		return new RetreiveMessageErrorDecoder();
+	}
+
+	@Bean
+	Logger.Level feignLoggerLevel() {
+		return Logger.Level.FULL;
+	}
 }
