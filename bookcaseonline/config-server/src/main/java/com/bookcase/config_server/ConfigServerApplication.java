303171d6aa91448026c8a8377d0a309939f6b8a4
burakTuran00 <burakturan7410@gmail.com>
2025-11-01 18:22:13 +0300

    Spring Cloud Config

---
+++ bookcaseonline/config-server/src/main/java/com/bookcase/config_server/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.bookcase.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
